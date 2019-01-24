22502511f4d423c39ace38ca0c96094c31ebdba4
Salvatore Scrudato <salscrudato@Salvatores-MacBook-Air.local>
2019-01-24 09:42:28 -0800

    changed db config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-	database: 'mongodb://admin:admin123@ds141252-a0.mlab.com:41252,ds141252-a1.mlab.com:41252/slatechamp?replicaSet=rs-ds141252',
+	//database: 'mongodb://admin:admin123@ds141252-a0.mlab.com:41252,ds141252-a1.mlab.com:41252/slatechamp?replicaSet=rs-ds141252',
+	database: 'mongodb://slatechamp:Salscru516@ds036069.mlab.com:36069/saltest',
 	secret: 'yoursecret'
 }
